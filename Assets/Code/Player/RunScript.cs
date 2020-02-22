5b7859ab14f2940c4d2f1dbc06127bef52c7bcd4
virot <virotor.vik@gmail.com>
2020-02-23 00:40:11 +0300

    Update .gitignore

---
+++ Assets/Code/Player/RunScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+[RequireComponent (typeof(Player))] 
+
+public class RunScript : MonoBehaviour
+{
+
+    public Player Player;
+
+    private void Start()
+    {
+        
+        Player = GetComponent<Player>();
+       
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 1)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                Player.Jump();
+            if(Input.GetKeyDown(KeyCode.F))
+                Player.Attack();
+            Player.Move();
+            Player.Damage();
+            Player.Health();
+        }
+       
+    }
+
+    private void FixedUpdate()
+    {
+        Player.Damage();
+    }
+
+
+}
